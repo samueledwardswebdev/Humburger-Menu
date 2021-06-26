1173ecd5ee53f1b288bc2d17ce0c4f4bb053db4f
samueledwardswebdev <69369854+samueledwardswebdev@users.noreply.github.com>
2021-06-26 10:33:09 +0100

    2nd Upload

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+// const mainMenu = document.querySelector('.mainMenu');
+ // const closeMenu = document.querySelector('.closeMenu');
+ // const openMenu = document.querySelector('.openMenu');
+ const mainMenu = document.querySelector('.mainMenu');
+ const closeMenu = document.querySelector('.closeMenu');
+ const openMenu = document.querySelector('.openMenu');
+ const spinIcon = document.getElementById('close');
 
 
 
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+ // openMenu.addEventListener('click',show);
+ // closeMenu.addEventListener('click',close);
+ openMenu.addEventListener('click',show);
+ closeMenu.addEventListener('click',close);
+ spinIcon.addEventListener('click',spin);
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
+
+ function show(){
+     mainMenu.style.display = 'flex';
+     mainMenu.style.top = '0';
+ }
+ function close(){
+     mainMenu.style.top = '-100%';
+ }
+
+ function spin() {
+   spinIcon.style.animation = 'spin 4s infinite linear';
+ }
